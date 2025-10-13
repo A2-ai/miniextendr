30da98115a95ee7a4d54d90fc234c3700aeb4b1e
Mossa <cgmossa@gmail.com>
2025-10-13 03:13:53 +0200

    configure.ac: generates entrypoint.c.in if missning. then generates entrypoint.c

--- rpkg/src/entrypoint.c
+++ rpkg/src/entrypoint.c
@@ -2,12 +2,11 @@
 #include <Rinternals.h>
 #include <R_ext/Rdynload.h>
 
-
 extern void R_init_rpkg_miniextendr(DllInfo * dll);
 
 void R_init_rpkg(DllInfo *dll) {
     R_init_rpkg_miniextendr(dll);
-    
+
     R_useDynamicSymbols(dll, FALSE);
     R_forceSymbols(dll, TRUE);
 }
