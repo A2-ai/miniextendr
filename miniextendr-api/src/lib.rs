c85d3da94a9fa4265e72a41d7cbeeb2ce8f69dc5
Mossa <cgmossa@gmail.com>
2025-10-21 20:07:31 +0200

    wp: use common buffer for Rprintf, etc.

--- miniextendr-api/src/lib.rs
+++ miniextendr-api/src/lib.rs
@@ -58,6 +58,19 @@ pub use miniextendr_macros::miniextendr_module;
 
 pub mod ffi;
 pub mod unwind;
+pub mod error {
+    use crate::ffi::Rprintf;
+    use std::{
+        cell::RefCell,
+        ffi::{CStr, CString},
+    };
+
+    // work-in-progress: Use common buffer for the *const char APIs..
+    // thread_local! {
+    //     /// Buffer using in `rprintln`/`rprint`/`rerror`
+    //     pub static R_MESSAGE_BUFFER: RefCell<Vec<u8>> = RefCell::new(Vec::with_capacity(256));
+    // }
+}
 
 pub mod dots {
     use crate::ffi::SEXP;
